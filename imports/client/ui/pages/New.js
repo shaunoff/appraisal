8a64c22f0caa7444309ebbc4290c3afa77ee3c5d
Shaun Hutch <shutch@p3i-inc.com>
2017-02-08 13:38:39 -0500

    dashborad update

--- imports/client/ui/pages/New.js
+++ imports/client/ui/pages/New.js
@@ -12,38 +12,41 @@ const data = {
 	datasets: [{
 		data: [300, 50, 100],
 		backgroundColor: [
-		'#5BC85B',
-		'#FFC125',
-		'#D93A32'
+		'#007681',
+		'#6bada7',
+		'#fa7606'
 		],
 		hoverBackgroundColor: [
-		'#5BC85B',
-		'#FFC125',
-		'#D93A32'
+		'#007681',
+		'#6bada7',
+		'#fa7606'
 		]
 	}]
 };
+const warning = "#fa7606"
+const manager = "#6bada7"
+const primary = "#007681"
 const New = () =>
   <div style={{display: 'flex',  flexWrap:'wrap',fontWeight: '700', padding: '22px'}}>
 
       <div style={{flex: '3', display: 'flex',flexDirection: 'column',border: "2px solid #ccc", borderRadius: '8px',marginBottom: '22px',  marginRight: '11px', background: "white",minWidth: '60%'}}>
-          <div style={{fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
+          <div style={{fontSize: "18px", fontWeight: '700', color: '#6bada7',padding:"10px"}}>
             Upcoming Deadlines
           </div>
-          <hr style={{marginLeft: "10px",marginRight: "10px", marginTop: '0px', border: '0px solid #ccc', height: '2px',background: '#ccc', borderRadius: '2px'}}/>
+
           <div style={{display: 'flex'}}>
             <div style={{ minWidth: '150px',display: 'flex', flexDirection: 'column', alignItems: 'left',justifyContent: 'center', marginLeft: '15px'}}>
               <div style={{display: 'flex',  alignItems: 'center',justifyContent: 'flex-start',marginBottom: '10px'}}>
-                <div style={{width: '20px', height: '20px',borderRadius: '30px', background: "#5BC85B",marginRight: '8px'}}></div>
-                  <div style={{fontWeight: '500', color: '#585858' }}>: Complete</div>
+                <div style={{width: '20px', height: '20px',borderRadius: '30px', background: "#007681",marginRight: '8px'}}></div>
+                  <div style={{fontWeight: '500', color: '#007681' }}>: Complete</div>
               </div>
               <div style={{display: 'flex',  alignItems: 'center', justifyContent: 'flex-start',marginBottom: '10px'}}>
-                <div style={{width: '20px', height: '20px',borderRadius: '30px', background: "#FFC125",marginRight: '8px'}}></div>
-                  <div style={{fontWeight: '500', color: '#585858' }}>: Incomplete</div>
+                <div style={{width: '20px', height: '20px',borderRadius: '30px', background: "#fa7606",marginRight: '8px'}}></div>
+                  <div style={{fontWeight: '500', color: '#fa7606' }}>: Incomplete</div>
               </div>
               <div style={{display: 'flex',  alignItems: 'center', justifyContent: 'flex-start',marginBottom: '10px'}}>
-                <div style={{width: '20px', height: '20px',borderRadius: '30px', background: "red",marginRight: '8px'}}></div>
-                  <div style={{fontWeight: '500', color: '#585858' }}>: In Progress</div>
+                <div style={{width: '20px', height: '20px',borderRadius: '30px', background: "#6bada7",marginRight: '8px'}}></div>
+                  <div style={{fontWeight: '500', color: '#6bada7' }}>: In Progress</div>
               </div>
 
             </div>
@@ -66,18 +69,18 @@ const New = () =>
                 <div style={{marginLeft: '15px'}}>Appraisal Progress</div>
                 <div style={{padding: '10px',display: 'flex', alignItems: 'center',  alignItems: 'center', margin: '10px',border: '2px solid #ccc',borderRadius: '8px', height: '60px'}}>
                   <div style={{display: 'flex', flexDirection: 'column',flex:'1', alignItems: 'center',justifyContent: 'center',}}>
-                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '40px', height: '40px',background:'white',borderRadius: '40px', border: '2px solid #5BC85B',marginRight: '8px',zIndex: '3'}}>
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '40px', height: '40px',background:'white',borderRadius: '40px', border: '2px solid #007681',marginRight: '8px',zIndex: '3'}}>
                       <img style={{width: '15px'}} src="/icons/checked.svg"/>
                     </div>
-                    <div style={{fontSize: '12px', flex: '1',color: '#5BC85B'}}>Self-Assessment</div>
-                    <div style={{flex: '1',height: '2px', width: '50%', background:'#5BC85B',position: 'relative', top: '-40px',marginLeft: '50%',zIndex: '2'}}></div>
+                    <div style={{fontSize: '12px', flex: '1',color: '#007681'}}>Self-Assessment</div>
+                    <div style={{flex: '1',height: '2px', width: '50%', background:'#007681',position: 'relative', top: '-40px',marginLeft: '50%',zIndex: '2'}}></div>
                   </div>
                   <div style={{display: 'flex', flexDirection: 'column',flex:'1', alignItems: 'center',justifyContent: 'center',}}>
-                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',width: '40px', height: '40px',borderRadius: '40px', border: '2px solid #007681', background: "white",marginRight: '8px',zIndex: '3'}}>
-                      <div style={{color: "#007681",fontSize: '20px',fontWeight: '700'}}>2</div>
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',width: '40px', height: '40px',borderRadius: '40px', border: '2px solid #6bada7', background: "white",marginRight: '8px',zIndex: '3'}}>
+                      <div style={{color: "#6bada7",fontSize: '20px',fontWeight: '700'}}>2</div>
                     </div>
-                    <div style={{fontSize: '12px',fontWeight: '700',color: '#007681'}}>Manager Assessment</div>
-                    <div style={{flex: '1',height: '2px', width: '100%', background:'#007681',position: 'relative', top: '-40px',zIndex: '2'}}>
+                    <div style={{fontSize: '12px',fontWeight: '700',color: '#6bada7'}}>Manager Assessment</div>
+                    <div style={{flex: '1',height: '2px', width: '100%', background:'#6bada7',position: 'relative', top: '-40px',zIndex: '2'}}>
 
                     </div>
                   </div>
@@ -101,11 +104,11 @@ const New = () =>
                     <div style={{flex: '1',height: '2px', width: '50%', background:'#5BC85B',position: 'relative', top: '-40px',marginLeft: '50%',zIndex: '2'}}></div>
                   </div>
                   <div style={{display: 'flex', flexDirection: 'column',flex:'1', alignItems: 'center',justifyContent: 'center',}}>
-                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '40px', height: '40px',background:'white',borderRadius: '40px', border: '2px solid #D93A32',marginRight: '8px',zIndex: '3'}}>
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '40px', height: '40px',background:'white',borderRadius: '40px', border: '2px solid #fa7606',marginRight: '8px',zIndex: '3'}}>
                       <img style={{width: '15px'}} src="/icons/unchecked.svg"/>
                     </div>
-                    <div style={{fontSize: '12px',fontWeight: '700',color: '#D93A32'}}>Meet Target</div>
-                    <div style={{flex: '1',height: '2px', width: '100%', background:'#D93A32',position: 'relative', top: '-40px',zIndex: '2'}}>
+                    <div style={{fontSize: '12px',fontWeight: '700',color: '#fa7606'}}>Meet Target</div>
+                    <div style={{flex: '1',height: '2px', width: '100%', background:'#fa7606',position: 'relative', top: '-40px',zIndex: '2'}}>
 
                     </div>
                   </div>
@@ -122,18 +125,18 @@ const New = () =>
         </div>
       </div>
       <div style={{flex: '1', display: 'flex',flexDirection: 'column',border: "2px solid #ccc", borderRadius: '8px', marginBottom: '22px',marginLeft: '11px', background: "white",minWidth: '30%'}}>
-          <div style={{fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
+          <div style={{fontSize: "18px", fontWeight: '700', color: '#6bada7',padding:"10px"}}>
             Upcoming Deadlines
           </div>
-          <hr style={{marginLeft: "10px",marginRight: "10px", marginTop: '0px', border: '0px solid #ccc', height: '2px',background: '#ccc', borderRadius: '2px'}}/>
+
           <Doughnut data={data} />
       </div>
       <div style={{flex: '1', display: 'flex',flexDirection: 'column',border: "2px solid #ccc", borderRadius: '8px', marginRight: '11px', background: "white"}}>
-          <div style={{fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
+          <div style={{fontSize: "18px", fontWeight: '700', color: '#6bada7',padding:"10px"}}>
             Upcoming Deadlines
 
           </div>
-          <hr style={{marginLeft: "10px",marginRight: "10px", marginTop: '0px', border: '0px solid #ccc', height: '2px',background: '#ccc', borderRadius: '2px'}}/>
+
           <div style={{display: 'flex', alignItems: 'center', margin: '10px',border: '1px solid #ccc',borderLeft: "6px solid #6bada7", height: '60px'}}>
             <img style={{marginLeft: "15px", width: '25px'}} src="/icons/target.svg"/>
             <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
@@ -164,45 +167,97 @@ const New = () =>
       </div>
 
       <div style={{flex: '1',display: 'flex',flexDirection: 'column',border: "2px solid #ccc", borderRadius: '8px', marginLeft: '11px', background: "white"}}>
-          <div style={{fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
+          <div style={{fontSize: "18px", fontWeight: '700', color: '#6bada7',padding:"10px"}}>
             Recent Activity
 
           </div>
-          <hr style={{marginLeft: "10px",marginRight: "10px", marginTop: '0px', border: '0px solid #ccc', height: '2px',background: '#ccc', borderRadius: '2px'}}/>
-          <div style={{display: 'flex', alignItems: 'center', margin: '10px',border: '1px solid #ccc',borderLeft: "6px solid #6bada7", height: '60px'}}>
-            <div style={{margin: '15px',display:'flex',fontWeight: '500',height: "40px",width: "40px", borderRadius: '40px',alignItems:'center',justifyContent: "center", background: '#007681', color: 'white'}}>
 
-                <div style={{ fontSize: '16px',color: 'white'}}>SH</div>
+					<div style={{display: 'flex', alignItems: 'center', margin: '10px',marginBottom: '5px',border: '1px solid #ccc',
+						borderRadius: '4px', height: '40px'}}>
+            <div style={{margin: '10px',display:'flex',fontWeight: '500',height: "30px",width: "30px", borderRadius: '30px',alignItems:'center',justifyContent: "center", background: primary, color: 'white'}}>
+
+                <div style={{ fontSize: '14px',color: 'white'}}>SH</div>
 
             </div>
-            <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
-               Shaun Hutch
+            <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: primary,padding:"10px"}}>
+               You
             </div>
             <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
-               Appraisal Complete
+               Approved Appraisal
             </div>
             <div style={{fontSize: "16px", fontWeight: '700', color: '#D93A32',padding:"10px"}}>
-              12 Jan
+              14 Jan
             </div>
           </div>
-          <div style={{display: 'flex', alignItems: 'center', margin: '10px',border: '1px solid #ccc',borderLeft: "6px solid #6bada7", height: '60px'}}>
-            <img style={{marginLeft: "15px", width: '25px'}} src="/icons/target.svg"/>
+					<div style={{display: 'flex', alignItems: 'center', margin: '10px',marginBottom: '5px',border: '1px solid #ccc',
+						borderRadius: '4px', height: '40px'}}>
+            <div style={{margin: '10px',display:'flex',fontWeight: '500',height: "30px",width: "30px", borderRadius: '30px',alignItems:'center',justifyContent: "center", background: manager, color: 'white'}}>
+
+                <div style={{ fontSize: '14px',color: 'white'}}>RG</div>
+
+            </div>
+            <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: manager,padding:"10px"}}>
+               Roger Goudreau
+            </div>
             <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
-               2017 Q3 Targets
+               Approved Appraisal
             </div>
             <div style={{fontSize: "16px", fontWeight: '700', color: '#D93A32',padding:"10px"}}>
-              3 Days
+              14 Jan
             </div>
           </div>
-          <div style={{display: 'flex', alignItems: 'center', margin: '10px',border: '1px solid #ccc',borderLeft: "6px solid #6bada7", height: '60px'}}>
-            <img style={{marginLeft: "15px", width: '25px'}} src="/icons/target.svg"/>
+					<div style={{display: 'flex', alignItems: 'center', margin: '10px',marginBottom: '5px',border: '1px solid #ccc',
+						borderRadius: '4px', height: '40px'}}>
+            <div style={{margin: '10px',display:'flex',fontWeight: '500',height: "30px",width: "30px", borderRadius: '30px',alignItems:'center',justifyContent: "center", background: manager, color: 'white'}}>
+
+                <div style={{ fontSize: '14px',color: 'white'}}>JG</div>
+
+            </div>
+            <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: manager,padding:"10px"}}>
+               Jan Guy
+            </div>
             <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
-               2017 Q3 Targets
+               Final Approval
             </div>
             <div style={{fontSize: "16px", fontWeight: '700', color: '#D93A32',padding:"10px"}}>
-              3 Days
+              14 Jan
             </div>
           </div>
+					<div style={{display: 'flex', alignItems: 'center', margin: '10px',marginBottom: '5px',border: '1px solid #ccc',
+						borderRadius: '4px', height: '40px'}}>
+            <div style={{margin: '10px',display:'flex',fontWeight: '500',height: "30px",width: "30px", borderRadius: '30px',alignItems:'center',justifyContent: "center", background: primary, color: 'white'}}>
+
+                <div style={{ fontSize: '14px',color: 'white'}}>SH</div>
+
+            </div>
+            <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: primary,padding:"10px"}}>
+               You
+            </div>
+            <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
+               Approved Appraisal
+            </div>
+            <div style={{fontSize: "16px", fontWeight: '700', color: '#D93A32',padding:"10px"}}>
+              14 Jan
+            </div>
+          </div>
+					<div style={{display: 'flex', alignItems: 'center', margin: '10px',marginBottom: '5px',border: '1px solid #ccc',
+						borderRadius: '4px', height: '40px'}}>
+            <div style={{margin: '10px',display:'flex',fontWeight: '500',height: "30px",width: "30px", borderRadius: '30px',alignItems:'center',justifyContent: "center", background: primary, color: 'white'}}>
+
+                <div style={{ fontSize: '14px',color: 'white'}}>SH</div>
+
+            </div>
+            <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: primary,padding:"10px"}}>
+               You
+            </div>
+            <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
+               Approved Appraisal
+            </div>
+            <div style={{fontSize: "16px", fontWeight: '700', color: '#D93A32',padding:"10px"}}>
+              14 Jan
+            </div>
+          </div>
+
       </div>
       <div style={{flex: '1',display: 'flex',flexDirection: 'column',border: "2px solid #ccc", borderRadius: '8px', marginLeft: '11px', background: "white"}}>
           <div style={{fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
@@ -222,7 +277,7 @@ const New = () =>
             <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
                Appraisal Complete
             </div>
-            <div style={{fontSize: "16px", fontWeight: '700', color: '#D93A32',padding:"10px"}}>
+            <div style={{fontSize: "16px", fontWeight: '700', color: warning,padding:"10px"}}>
               12 Jan
             </div>
           </div>
@@ -231,7 +286,7 @@ const New = () =>
             <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
                2017 Q3 Targets
             </div>
-            <div style={{fontSize: "16px", fontWeight: '700', color: '#D93A32',padding:"10px"}}>
+            <div style={{fontSize: "16px", fontWeight: '700', color: warning,padding:"10px"}}>
               3 Days
             </div>
           </div>
@@ -240,7 +295,7 @@ const New = () =>
             <div style={{flex: '1', fontSize: "16px", fontWeight: '700', color: '#585858',padding:"10px"}}>
                2017 Q3 Targets
             </div>
-            <div style={{fontSize: "16px", fontWeight: '700', color: '#D93A32',padding:"10px"}}>
+            <div style={{fontSize: "16px", fontWeight: '700', color: warning,padding:"10px"}}>
               3 Days
             </div>
           </div>
